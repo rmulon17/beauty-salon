e18279490e2a91267cfb1b17af3e43bc20461b5a
rmulon17 <rmulon17@gmail.com>
2022-06-07 10:44:24 -0300

    Adiciona funcionalidade que mostra os elementos ao dar scroll na página

--- main.js
+++ main.js
@@ -39,3 +39,18 @@ const swiper = new Swiper('.swiper-container', {
   mousewheel: true,
   keyboard: true
 });
+
+
+/* ScrollReveal: mostrar elementos quando der scroll na página */
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+});
+
+scrollReveal.reveal(`#home .image, #home .text, 
+#about .image, #about .text,
+#services header, #services .card,
+#testimonials header, #testimonials .testimonials,
+#contact .text, #contact .links`, { interval: 100 });
