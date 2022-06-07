3a247d32d9d81863355975d983f5dab1c6bcd6d2
rmulon17 <rmulon17@gmail.com>
2022-06-07 13:32:11 -0300

    Adiciona funcionalidade de voltar ao topo e adiciona transição nos links sociais do rodapé

--- main.js
+++ main.js
@@ -53,4 +53,16 @@ scrollReveal.reveal(`#home .image, #home .text,
 #about .image, #about .text,
 #services header, #services .card,
 #testimonials header, #testimonials .testimonials,
-#contact .text, #contact .links`, { interval: 100 });
+#contact .text, #contact .links,
+footer .brand, footer .social`, { interval: 100 });
+
+
+/* Botão voltar para o topo */
+const backToTopButton = document.querySelector('.back-to-top');
+window.addEventListener('scroll', function() {
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show');
+  } else {
+    backToTopButton.classList.remove('show');
+  }
+});
